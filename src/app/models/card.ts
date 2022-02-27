c424c2cb05eaae3b58d04c3243e456bfb20bf59d
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-02-27 15:16:35 +0300

    date & place infos added

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -2,10 +2,14 @@ export class Card {
   id: string;
   img: string;
   title: string;
+  date: string;
+  place: string;
 
   constructor(item: any) {
     this.id = item.id || '';
     this.img = item.img || '';
     this.title = item.title || '';
+    this.date = item.date || '';
+    this.place = item.place || '';
   }
 }
